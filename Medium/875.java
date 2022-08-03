509bd9e513edb1bd994fb3a5b81205dbac2fe846
LeVi4869 <thanhvinhle3900@gmail.com>
2022-08-03 20:55:15 +0700

    adding 875

---
+++ Medium/875.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int H) {
+        int lo = 1, hi = getMaxPile(piles);
+        
+        // Binary search to find the smallest valid K.
+        while (lo <= hi) {
+            int K = lo + ((hi - lo) >> 1);
+            if (canEatAll(piles, K, H)) {
+                hi = K - 1;
+            } else {
+                lo = K + 1;
+            }
+        }
+        
+        return lo;
+    }
+    
+    private boolean canEatAll(int[] piles, int K, int H) {
+        long countHour = 0; // Hours take to eat all bananas at speed K.
+        
+        for (int pile : piles) {
+            countHour += pile / K;
+            if (pile % K != 0)
+                countHour++;
+        }
+        return countHour <= H;
+    }
+    
+    private int getMaxPile(int[] piles) {
+        int maxPile = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            maxPile = Math.max(pile, maxPile);
+        }
+        return maxPile;
+    }
+}
