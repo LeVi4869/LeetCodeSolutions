38943e266815d41e6dfde59f7f9cc87cdc472874
LeVi4869 <thanhvinhle3900@gmail.com>
2022-08-17 22:34:39 +0700

    changing 45 to a better solution

--- Medium/45.java
+++ Medium/45.java
@@ -1,15 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j <= nums[i] && i + j < nums.length; j++) {
-                if (dp[i + j] == 0) {
-                    dp[i + j] = dp[i] + 1;
-                } else {
-                    dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
-                }
+        int furthest = 0;
+        int cnt = 0;
+        int curr = 0;
+        while(curr < nums.length)   {
+            if(furthest >= nums.length - 1) {
+                return cnt;
             }
+            int max = furthest;
+            for(int i = curr; i <= max; ++i)   {
+                furthest = Math.max(furthest, i + nums[i]);
+            }
+            ++cnt;
         }
-        return dp[nums.length - 1];
+        return cnt;
     }
 }
