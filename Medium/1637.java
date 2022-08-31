7fe9f550c2e36213bbcc89929a13110c0544b299
LeVi4869 <thanhvinhle3900@gmail.com>
2022-08-31 10:14:43 +0700

    adding 1637(M)

---
+++ Medium/1637.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(p -> p[0]));
+        int res = 0;
+        for(int i = 1; i < points.length; ++i){
+            res = Math.max(res, points[i][0] - points[i - 1][0]);
+        }
+        return res;
+    }
+}
