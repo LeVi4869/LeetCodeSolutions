53ecb5cb220dec9b621655bc083ce7a5fd2c52ab
LeVi4869 <thanhvinhle3900@gmail.com>
2023-01-09 12:10:30 -0400

    adding 224(M)

---
+++ Medium/224.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int total = 0;
+        total += (ax2 - ax1) * (ay2 - ay1);
+        total += (bx2 - bx1) * (by2 - by1);
+        int xOverlap = Math.min(ax2,bx2) - Math.max(ax1,bx1);
+        int yOverlap = Math.min(ay2,by2) - Math.max(ay1, by1);
+        if(xOverlap > 0 && yOverlap > 0){
+            total -= (xOverlap * yOverlap);
+        }
+        return total;
+    }
+}
