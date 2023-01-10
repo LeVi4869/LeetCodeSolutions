aab2a557f9bc5edd4bf260e01e0cc1556d255766
LeVi4869 <thanhvinhle3900@gmail.com>
2023-01-10 14:10:00 -0400

    adding 2428(M)

---
+++ Medium/2428.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int res = Integer.MIN_VALUE;
+        for(int i = 0; i < grid.length - 2; ++i){
+            for(int j = 0; j < grid[0].length - 2; ++j){
+                res = Math.max(res, grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]);
+            }
+        }
+        return res;
+    }
+}
